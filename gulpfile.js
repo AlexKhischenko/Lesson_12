7fc13b1a57bbeb1b3566a9ee57cb09a0b0633028
Alex Khischenko <Khischenko.A@gmail.com>
2020-03-04 22:07:32 +0200

    feat: added gulp-sass in gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -1,22 +1,60 @@
-const gulp = require('gulp');
+// const gulp = require('gulp');
+// const browserSync = require('browser-sync').create();
+// const cleanCSS = require('gulp-clean-css');
+// const rename = require("gulp-rename");
+
+// // Static server
+// gulp.task('browser-sync', function() {
+//   browserSync.init({
+//       server: {
+//           baseDir: "./"
+//       }
+//   });
+//   gulp.watch("./*.html").on('change', browserSync.reload);
+// });
+
+// // Minify and rename CSS files
+// gulp.task('mincss', function() {
+//   return gulp.src("./css/*.css")  
+//   .pipe(rename({suffix: ".min"}))  
+//   .pipe(cleanCSS())  
+//   .pipe(gulp.dest("./css"));  
+// });
+
+
+const {src, dest, watch} = require('gulp');
 const browserSync = require('browser-sync').create();
+const sass = require('gulp-sass');
 const cleanCSS = require('gulp-clean-css');
 const rename = require("gulp-rename");
 
 // Static server
-gulp.task('browser-sync', function() {
+function bs() {
+  serveSass();
   browserSync.init({
       server: {
-          baseDir: "./"
+          baseDir: "./src"
       }
   });
-  gulp.watch("./*.html").on('change', browserSync.reload);
-});
+  watch("./src/*.html").on('change', browserSync.reload);
+  watch("./src/sass/**/*.sass", serveSass);
+  watch("./src/js/*.js").on('change', browserSync.reload);
+};
+
+// Sass compile in CSS
+function serveSass() {
+  return src('./src/sass/*.sass')
+      .pipe(sass())
+      .pipe(dest('./src/css'))
+      .pipe(browserSync.stream());
+};
 
 // Minify and rename CSS files
-gulp.task('mincss', function() {
-  return gulp.src("./css/*.css")  
+function minCSS() {
+  return src("./src/css/*.css")  
   .pipe(rename({suffix: ".min"}))  
   .pipe(cleanCSS())  
-  .pipe(gulp.dest("./css"));  
-});
+  .pipe(dest("./src/css"));  
+};
+
+exports.serve = bs;
