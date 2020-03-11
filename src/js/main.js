0fae17754392372807adbe63bd963b47fea85744
Alex Khischenko <Khischenko.A@gmail.com>
2020-03-11 18:58:06 +0200

    feat: added js file main.js, added functions for modal

--- src/js/main.js
+++ src/js/main.js
@@ -2,13 +2,34 @@ document.addEventListener("DOMContentLoaded", function(event) {
   const modal = document.querySelector('.modal');
   const modalBtn = document.querySelectorAll('[data-toggle=modal]');
   const closeBtn = document.querySelector('.modal__close');
+  
+  // Функция добавляет класс "modal--visible"
   const switchModal = () => {
     modal.classList.toggle('modal--visible');
-  }
-  console.log(modalBtn);
+  };
+
+  // Функция закрывает модальное окно клавишей "Escape"
+  const escapeBtn = (event) => {
+    if (modal.classList.contains('modal--visible')) {
+      if (event.which == 27) {
+        switchModal();
+      };
+    };
+  };
+
+  // Функция закрывает модальное окно при клике вне модального окна
+  const clickFreeSpace = (e) => {
+    if (e.target.classList.contains('modal')) {
+      switchModal();
+    };
+  };
 
   modalBtn.forEach(element => {
     element.addEventListener('click', switchModal);
   });
+
   closeBtn.addEventListener('click', switchModal);
+  closeBtn.addEventListener('click', switchModal);
+  document.addEventListener('keydown', escapeBtn);  
+  document.addEventListener('click', clickFreeSpace);
 });
