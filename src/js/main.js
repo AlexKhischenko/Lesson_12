03002d572534c395681b44675f912b1d6cadaaf8
Alex Khischenko <Khischenko.A@gmail.com>
2020-03-12 11:30:11 +0200

    refact: changed js on jquery

--- src/js/main.js
+++ src/js/main.js
@@ -1,4 +1,4 @@
-document.addEventListener("DOMContentLoaded", function(event) {
+/*document.addEventListener("DOMContentLoaded", function(event) {
   const modal = document.querySelector('.modal');
   const modalBtn = document.querySelectorAll('[data-toggle=modal]');
   const closeBtn = document.querySelector('.modal__close');
@@ -32,4 +32,46 @@ document.addEventListener("DOMContentLoaded", function(event) {
   closeBtn.addEventListener('click', switchModal);
   document.addEventListener('keydown', escapeBtn);  
   modal.addEventListener('click', clickFreeSpace);
+}); */
+
+/*$(document).ready(function () {
+  let modal = $('.modal');
+  let modalBtn = $('[data-toggle=modal]');
+  let closeBtn = $('.modal__close');
+
+  modalBtn.on('click', function() {
+    modal.toggleClass('modal--visible');
+  });
+  closeBtn.on('click', function() {
+    modal.toggleClass('modal--visible');
+  });
+
+}); */
+
+$(document).ready(function () {
+  let modal = $('.modal');
+  let modalBtn = $('[data-toggle=modal]');
+  let closeBtn = $('.modal__close');
+
+  modalBtn.click( () => {
+    modal.toggleClass('modal--visible');
+  });
+  
+  closeBtn.click( () => {
+    modal.toggleClass('modal--visible');
+  });
+
+  $(document).keydown((event) => {
+    if (modal.hasClass('modal--visible')) {
+      if (event.which == 27) {
+        modal.toggleClass('modal--visible');
+      };
+    };
+  });
+
+  modal.click((e) => {
+    if (modal.is(e.target) && modal.has(e.target).length === 0) {
+      modal.toggleClass('modal--visible');
+    };
+  });
 });
