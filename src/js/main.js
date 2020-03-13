6b18a2ef212274ed0d0900caf823dfd6ceb2bb31
Alex Khischenko <Khischenko.A@gmail.com>
2020-03-13 18:03:29 +0200

    feat: added projects section

--- src/js/main.js
+++ src/js/main.js
@@ -90,4 +90,24 @@ $(document).ready(function () {
     $('html').animate({scrollTop : 0}, 900);
 });
 
+//initialize swiper when document ready
+var mySwiper = new Swiper ('.swiper-container', {
+  loop: true,
+  pagination: {
+    el: '.swiper-pagination',
+    type: 'bullets',
+  },
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+});
+
+var next = $('.swiper-button-next');
+var prev = $('.swiper-button-prev');
+var bullets = $('.swiper-pagination');
+
+next.css('left', prev.width() + 20 + bullets.width() + 20);
+bullets.css('left', prev.width() + 20);
+
 });
