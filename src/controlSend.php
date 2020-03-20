e162f942af91092342a84182ed32de2894ed6bc4
Alex Khischenko <Khischenko.A@gmail.com>
2020-03-20 19:19:11 +0200

    refact: emails send correctly

--- src/controlSend.php
+++ src/controlSend.php
@@ -18,13 +18,13 @@ try {
     $mail->isSMTP();                                  // Send using SMTP
     $mail->Host       = 'smtp.gmail.com';             // Set the SMTP server to send through
     $mail->SMTPAuth   = true;                         // Enable SMTP authentication
-    $mail->Username   = 'khischenko.a@gmail.com';     // SMTP username
+    $mail->Username   = 'webdev.akh@gmail.com';     // SMTP username
     $mail->Password   = '!123Qqwe';                   // SMTP password
     $mail->SMTPSecure = 'ssl';                        // Enable TLS encryption;
     $mail->Port       = 465;                          // TCP port to connect to
 
     //Recipients
-    $mail->setFrom('khischenko.a@gmail.com');
+    $mail->setFrom('webdev.akh@gmail.com');
     $mail->addAddress('khischenkoas@dex-ua.com');      // Add a recipient
 
     // Content
--- src/modalSend.php
+++ src/modalSend.php
@@ -5,9 +5,9 @@ $modalUserEmail = $_POST['modalUserEmail'];
 $modalUserPhone = $_POST['modalUserPhone'];
 
 // Load Composer's autoloader
+require 'phpmailer/Exception.php';
 require 'phpmailer/PHPMailer.php';
 require 'phpmailer/SMTP.php';
-require 'phpmailer/Exception.php';
 
 // Instantiation and passing `true` enables exceptions
 $mail = new PHPMailer\PHPMailer\PHPMailer();
@@ -15,17 +15,17 @@ $mail->CharSet = "UTF-8";
 
 try {
     //Server settings
-    $mail->SMTPDebug = 0;                             // Enable verbose debug output
+    $mail->SMTPDebug = 2;                             // Enable verbose debug output
     $mail->isSMTP();                                  // Send using SMTP
-    $mail->Host       = 'smtp.gmail.com';             // Set the SMTP server to send through
+    $mail->Host       = 'smtp.gmail.com';                  // Set the SMTP server to send through
     $mail->SMTPAuth   = true;                         // Enable SMTP authentication
-    $mail->Username   = 'khischenko.a@gmail.com';     // SMTP username
+    $mail->Username   = 'webdev.akh@gmail.com';          // SMTP username
     $mail->Password   = '!123Qqwe';                   // SMTP password
     $mail->SMTPSecure = 'ssl';                        // Enable TLS encryption;
     $mail->Port       = 465;                          // TCP port to connect to
 
     //Recipients
-    $mail->setFrom('khischenko.a@gmail.com');
+    $mail->setFrom('webdev.akh@gmail.com');
     $mail->addAddress('khischenkoas@dex-ua.com');      // Add a recipient
     
     // Content
